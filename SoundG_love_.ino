099576651c687da4b0821e903a3b7032a7756ea1
verdut <34348051+verdut@users.noreply.github.com>
2018-03-03 21:27:38 +0200

    Update SoundG_love_.ino

--- SoundG_love_.ino
+++ SoundG_love_.ino
@@ -43,42 +43,42 @@ void tone1()     //alegerea tonurilor pentru degetul 1
            
         }       
    }
-                  else if (flexSensor1==2)
-                  {  
-                    if(isSinging==false)
-                         {  
-                          isSinging=true;
-                  
-                          if(isGuitar==true) 
-                            action=2;           //myDFPlayer.play(2);
-                              else action=17;   //myDFPlayer.play(17); 
-                          }     
-                    
-                     }
-                                  else if (flexSensor1==3)
-                                  {   
-                                    if(isSinging==false)
-                                         {  
-                                          isSinging=true;
-                                  
-                                          if(isGuitar==true) 
-                                            action=3;           //myDFPlayer.play(3);
-                                              else action=18;   //myDFPlayer.play(18); 
-                                          }     
-                                    
-                                     }
-                                                else
-                                                {
-                                                    if(isSinging==false)
-                                                       {  
-                                                        isSinging=true;
-                                                
-                                                        if(isGuitar==true) 
-                                                          action=1;           //myDFPlayer.play(1);
-                                                            else action=16;  //myDFPlayer.play(16); 
-                                                        }    
-                                                   
-                                                   }
+    else if (flexSensor1==2)
+    {  
+      if(isSinging==false)
+           {  
+            isSinging=true;
+
+            if(isGuitar==true) 
+              action=2;           //myDFPlayer.play(2);
+                else action=17;   //myDFPlayer.play(17); 
+            }     
+
+       }
+    else if (flexSensor1==3)
+    {   
+      if(isSinging==false)
+           {  
+            isSinging=true;
+
+            if(isGuitar==true) 
+              action=3;           //myDFPlayer.play(3);
+                else action=18;   //myDFPlayer.play(18); 
+            }     
+
+       }
+  else
+  {
+      if(isSinging==false)
+         {  
+          isSinging=true;
+
+          if(isGuitar==true) 
+            action=1;           //myDFPlayer.play(1);
+              else action=16;  //myDFPlayer.play(16); 
+          }    
+
+     }
 
    
 }
@@ -99,39 +99,39 @@ void tone2()                 //alegerea tonurilor pentru degetul 2
         }    
     
     }
-                else if (flexSensor2==2)
-                {
-                  if(isSinging==false)
-                       {  
-                        isSinging=true;
-                
-                        if(isGuitar==true) 
-                          action=5;               //myDFPlayer.play(5);
-                            else action=20;       //myDFPlayer.play(20); 
-                        }    
-                  }
-                                    else if (flexSensor2==3)
-                                    {
-                                      if(isSinging==false)
-                                           {  
-                                            isSinging=true;
-                                    
-                                            if(isGuitar==true) 
-                                              action=6;             //myDFPlayer.play(6);
-                                                else action=21;     //myDFPlayer.play(21); 
-                                            }   
-                                      }
-                                                        else
-                                                        {
-                                                          if(isSinging==false)
-                                                               {  
-                                                                isSinging=true;
-                                                        
-                                                                if(isGuitar==true) 
-                                                                  action=4 ;          //myDFPlayer.play(4);
-                                                                    else action=19;   //myDFPlayer.play(19); 
-                                                                }    
-                                                          }
+  else if (flexSensor2==2)
+  {
+    if(isSinging==false)
+         {  
+          isSinging=true;
+
+          if(isGuitar==true) 
+            action=5;               //myDFPlayer.play(5);
+              else action=20;       //myDFPlayer.play(20); 
+          }    
+    }
+  else if (flexSensor2==3)
+  {
+    if(isSinging==false)
+         {  
+          isSinging=true;
+
+          if(isGuitar==true) 
+            action=6;             //myDFPlayer.play(6);
+              else action=21;     //myDFPlayer.play(21); 
+          }   
+    }
+  else
+  {
+    if(isSinging==false)
+         {  
+          isSinging=true;
+
+          if(isGuitar==true) 
+            action=4 ;          //myDFPlayer.play(4);
+              else action=19;   //myDFPlayer.play(19); 
+          }    
+    }
   
 }
 
@@ -148,39 +148,39 @@ if (flexSensor3==1)
             else action=22;       //myDFPlayer.play(22); 
         }   
     }
-                else if (flexSensor3==2)
-                {
-                  if(isSinging==false)
-                       {  
-                        isSinging=true;
-                
-                        if(isGuitar==true) 
-                          action=8;                 //myDFPlayer.play(8);
-                            else action=23;         //myDFPlayer.play(23);
-                        }    
-                  }
-                              else if (flexSensor3==3)
-                              {
-                                if(isSinging==false)
-                                     {  
-                                      isSinging=true;
-                              
-                                      if(isGuitar==true) 
-                                        action=9;                 //myDFPlayer.play(9);
-                                          else action=24;         //myDFPlayer.play(24);
-                                      }    
-                                }
-                                              else
-                                              {
-                                                if(isSinging==false)
-                                                     {  
-                                                      isSinging=true;
-                                              
-                                                      if(isGuitar==true) 
-                                                        action=7;             //myDFPlayer.play(7);
-                                                          else action=22;     //myDFPlayer.play(22); 
-                                                      }    
-                                                }
+else if (flexSensor3==2)
+{
+  if(isSinging==false)
+       {  
+        isSinging=true;
+
+        if(isGuitar==true) 
+          action=8;                 //myDFPlayer.play(8);
+            else action=23;         //myDFPlayer.play(23);
+        }    
+  }
+    else if (flexSensor3==3)
+    {
+      if(isSinging==false)
+           {  
+            isSinging=true;
+
+            if(isGuitar==true) 
+              action=9;                 //myDFPlayer.play(9);
+                else action=24;         //myDFPlayer.play(24);
+            }    
+      }
+    else
+    {
+      if(isSinging==false)
+           {  
+            isSinging=true;
+
+            if(isGuitar==true) 
+              action=7;             //myDFPlayer.play(7);
+                else action=22;     //myDFPlayer.play(22); 
+            }    
+      }
 }
 
 
@@ -190,98 +190,98 @@ void tone4()                        //alegerea tonurilor pentru degetul 4
 
   if (flexSensor4==1)
    {
-    if(isSinging==false)
-       {  
-        isSinging=true;
+      if(isSinging==false)
+         {  
+          isSinging=true;
 
-        if(isGuitar==true) 
-          action=10;                //myDFPlayer.play(10);
-            else action=25;         //myDFPlayer.play(25); 
-        }    
-    }
-                        else if (flexSensor4==2)
-                        {
-                          if(isSinging==false)
-                               {  
-                                isSinging=true;
-                        
-                                if(isGuitar==true) 
-                                  action=11;              //myDFPlayer.play(11);
-                                    else action=26;       //myDFPlayer.play(26); 
-                                }    
-                          }
-                                    else if (flexSensor4==3)
-                                    {
-                                      if(isSinging==false)
-                                           {  
-                                            isSinging=true;
-                                    
-                                            if(isGuitar==true) 
-                                              action=12;              //myDFPlayer.play(12);
-                                                else action=27;         //myDFPlayer.play(27); 
-                                            }   
-                                      }
-                                                    else
-                                                    {
-                                                      if(isSinging==false)
-                                                           {  
-                                                            isSinging=true;
-                                                    
-                                                            if(isGuitar==true) 
-                                                              action=10;                //myDFPlayer.play(10);
-                                                                else action=25;         //myDFPlayer.play(25); 
-                                                            }    
-                                                      }
+          if(isGuitar==true) 
+            action=10;                //myDFPlayer.play(10);
+              else action=25;         //myDFPlayer.play(25); 
+          }    
+      }
+      else if (flexSensor4==2)
+      {
+        if(isSinging==false)
+             {  
+              isSinging=true;
+
+              if(isGuitar==true) 
+                action=11;              //myDFPlayer.play(11);
+                  else action=26;       //myDFPlayer.play(26); 
+              }    
+        }
+      else if (flexSensor4==3)
+      {
+        if(isSinging==false)
+             {  
+              isSinging=true;
+
+              if(isGuitar==true) 
+                action=12;              //myDFPlayer.play(12);
+                  else action=27;         //myDFPlayer.play(27); 
+              }   
+        }
+      else
+      {
+        if(isSinging==false)
+             {  
+              isSinging=true;
+
+              if(isGuitar==true) 
+                action=10;                //myDFPlayer.play(10);
+                  else action=25;         //myDFPlayer.play(25); 
+              }    
+        }
 }
 
 
 
 void tone5()                              //alegerea tonurilor pentru degetul 5
 {
-if (flexSensor5==1)
-   {
-    if(isSinging==false)
-       {  
-        isSinging=true;
+      if (flexSensor5==1)
+         {
+          if(isSinging==false)
+             {  
+              isSinging=true;
 
-        if(isGuitar==true) 
-          action=13;            //myDFPlayer.play(13);
-            else action=28;     //myDFPlayer.play(28); 
-        }  
-    }
-              else if (flexSensor5==2)
-              {
-                if(isSinging==false)
-                     {  
-                      isSinging=true;
-              
-                      if(isGuitar==true) 
-                        action=14;              //myDFPlayer.play(14);
-                          else action=29;       //myDFPlayer.play(29); 
-                      }     
-                }
-                              else if (flexSensor5==3)
-                              {
-                                if(isSinging==false)
-                                     {  
-                                      isSinging=true;
-                              
-                                      if(isGuitar==true) 
-                                        action=15;                //myDFPlayer.play(15);
-                                          else action=30;         //myDFPlayer.play(30); 
-                                      }     
-                                }
-                                                    else
-                                                    {
-                                                      if(isSinging==false)
-                                                           {  
-                                                            isSinging=true;
-                                                    
-                                                            if(isGuitar==true) 
-                                                              action=13;              //myDFPlayer.play(13);
-                                                                else action=28;       //myDFPlayer.play(28); 
-                                                            }    
-                                                      }
+              if(isGuitar==true) 
+                action=13;            //myDFPlayer.play(13);
+                  else action=28;     //myDFPlayer.play(28); 
+              }  
+          }
+        else if (flexSensor5==2)
+        {
+          if(isSinging==false)
+               {  
+                isSinging=true;
+
+                if(isGuitar==true) 
+                  action=14;              //myDFPlayer.play(14);
+                    else action=29;       //myDFPlayer.play(29); 
+                }     
+          }
+        else if (flexSensor5==3)
+        {
+          if(isSinging==false)
+               {  
+                isSinging=true;
+
+                if(isGuitar==true) 
+                  action=15;                //myDFPlayer.play(15);
+                    else action=30;         //myDFPlayer.play(30); 
+                }     
+          }
+          else
+          {
+            if(isSinging==false)
+                 {  
+                  isSinging=true;
+
+                  if(isGuitar==true) 
+                    action=13;              //myDFPlayer.play(13);
+                      else action=28;       //myDFPlayer.play(28); 
+                  }    
+            }
 }
 
 
